fedc7064abab3899a5a6eede0582f6335e74cb5f
happy-tony <60281710+Serdyuk-prog@users.noreply.github.com>
2024-02-12 14:53:59 +0700

    deploy ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { Services } from './App.types';
 import { DataTree } from './components/DataTree/DataTree';
+import db from "./mocks/db.json";
 
 function App() {
   const [services, setServices] = useState<Services[]>([]);
@@ -37,7 +38,11 @@ function App() {
   }
   
   useEffect(() => {
-    fetch('/api/services').then((res) => res.json()).then((data) => setServices(arrayToTree(data)));
+    // Убираю моки для деплоя, не смог завести MSW на gh-pages
+    // fetch('/api/services').then((res) => res.json()).then((data) => setServices(arrayToTree(data)));
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    setServices(arrayToTree(db.services));
   }, []);
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// import packageJson from '../package.json'
 
-async function enableMocking() {
-  const { worker } = await import('./mocks/browser')
-  return worker.start()
-}
+// Убираю моки для деплоя, не смог завести MSW на gh-pages
+// async function enableMocking() {
+//   const { worker } = await import('./mocks/browser')
+//   return worker.start({
+//     serviceWorker: {
+//       // Provide a custom worker script URL, taking
+//       // the "homepage" into account.
+//       url: `${packageJson.homepage}/mockServiceWorker.js`,
+//     },
+//   })
+// }
 
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-})
+// enableMocking().then(() => {
+//   ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>,
+//   )
+// })
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
